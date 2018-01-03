4279d4eea2548dc5f0b0e7ff9de1efd87092de25
Jozef Chraplewski <jozef@vzaar.com>
2018-01-03 21:43:40 +0100

    topbar menu fixed for ie11

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-	function toggleMenu(moaMenu) {
+  function toggleMenu(moaMenu) {
     if (moaMenu.className === "menu sticky") {
       moaMenu.className += " responsive";
     } else {
-    	moaMenu.className = "menu sticky";
+      moaMenu.className = "menu sticky";
     }
-	}
+  }
 
-	const navButtons = document.querySelectorAll(".menu span")
+  const navButtons = document.querySelectorAll(".menu span")
   const moaMenu = document.querySelector("#moa-menu");
 
-	navButtons.forEach(function(button){
-		const id = button.getAttribute("data-name")
-		button.addEventListener('click', function() {
-			toggleMenu(moaMenu)
+  for(var i=0; i < navButtons.length; i++) {
+    const button = navButtons[i]
+    const id = button.getAttribute("data-name")
+    button.addEventListener('click', function() {
+      toggleMenu(moaMenu)
 
-			document.getElementById(id).scrollIntoView({ 
+      document.getElementById(id).scrollIntoView({
         behavior: 'smooth',
         block: "start"
       })
-		})
-	})
+    })
+
+  }
 
   const moaButton = document.querySelector("#moa");
 
   moaButton.addEventListener("click", function() {
-		toggleMenu(moaMenu)
+    toggleMenu(moaMenu)
   })
 })
-
